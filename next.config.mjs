8e8ec1ea26b89afbc3865ccdd7b42d9ffb00996d
Abhinandan Subedi <devloperabhinandan@gmail.com \ git config --global init.defaultBranch main \ git config --global color.ui auto \ git config --global core.editor code>
2025-09-16 06:36:22 +0000

    next config error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['images.unsplash.com', 'your-cdn.com'],
-    formats: ['image/avif', 'image/webp'],
-  },
-};
+const nextConfig = {};
 
-module.exports = nextConfig;
+export default nextConfig;
