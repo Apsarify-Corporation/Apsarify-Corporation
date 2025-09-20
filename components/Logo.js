76bcc6640167e1b14b721d60bd031c0d4407aa04
Abhinandan Subedi <devloperabhinandan@gmail.com \ git config --global init.defaultBranch main \ git config --global color.ui auto \ git config --global core.editor code>
2025-09-20 06:46:07 +0000

    Navbar Logo Change

--- components/Logo.js
+++ components/Logo.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Logo({ className = "", ...props }) {
   return (
     <Image
-      src="/images/ABOUT.png" // Replace with your logo file if available
+      src="/favicon.png" // Replace with your logo file if available
       alt="Apsarify Logo"
       width={40}
       height={40}
